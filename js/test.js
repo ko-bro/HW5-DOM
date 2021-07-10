c9367fef2cdb786dba5fb7721adfac26efcc102d
Mykola Bronovytskyi <bronovytskyi.m@gmail.com>
2021-07-10 10:57:59 +0300

    clear test.js from comments

--- js/test.js
+++ js/test.js
@@ -1,33 +1,3 @@
-// let inputs = document.querySelectorAll('input');
-
-// loginBtn.onclick = function() {
-//    paragraph.textContent = inputs.value;
-// }
-
-
-// let inputs = document.querySelector('input');
-// console.log(inputs.value);
-
-// output.textContent = inputs.value;
-
-
-// let inputs = document.getElementsByTagName('input');
-
-// best
-// document.querySelector('button').onclick = logBtn;
-
-// function logBtn() {
-//     let inputs = document.querySelectorAll('input').value;
-//     document.querySelector('.form-output').innerHTML = inputs;
-// }
-
-
-
-// loginBtn.onClick = function() {
-//     let inputs = document.querySelector('input');
-//     document.querySelector('.paragraph').innerHTML = inputs;
-// }
-
 document.getElementById('loginBtn').onclick = logBtn;
 function logBtn() {
     let user = document.getElementById('username').value;
